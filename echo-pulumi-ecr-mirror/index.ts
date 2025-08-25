9789dfa4159d6693591035c04b69109c5f0f5514
Itay Wolfish <itay.wolfish@echohq.com>
2025-08-25 14:16:52 +0300

    update ecr and gar readmes

--- echo-pulumi-ecr-mirror/index.ts
+++ echo-pulumi-ecr-mirror/index.ts
@@ -80,7 +80,7 @@ export interface EcrPullThroughCacheOutputs {
  * export const usage = cache.usageInstruction;
  * ```
  */
-export class EcrPullThroughCache extends pulumi.ComponentResource {
+export default class EcrPullThroughCache extends pulumi.ComponentResource {
     public readonly cacheNamespace: pulumi.Output<string>;
     public readonly roleArn: pulumi.Output<string>;
     public readonly policyArn: pulumi.Output<string>;
