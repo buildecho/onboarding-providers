b15a49f23848ad70f205cf7a803ed430b1c9803d
Itay Wolfish <itay.wolfish@echohq.com>
2025-08-26 14:35:09 +0300

    fix ecr terraform

--- echo-terraform-ecr-mirror/outputs.tf
+++ echo-terraform-ecr-mirror/outputs.tf
@@ -7,12 +7,6 @@ output "role_arn" {
   value       = var.create ? try(aws_iam_role.ecr_access[0].arn, null) : null
 }
 
-output "policy_arn" {
-  description = "ARN of the ECR access policy"
-  value       = var.create ? try(aws_iam_role_policy.ecr_pullthrough_cache[0].policy_arn, null) : null
-}
-
-
 output "usage_instruction" {
   description = "Single-line usage instruction for pulling via the mirror"
   value       = var.create ? "docker pull ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.cache_namespace}/<image>:<tag>" : null
