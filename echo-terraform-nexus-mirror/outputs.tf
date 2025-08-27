dd68d01748bbc664e2e262c858adc51fc93f230a
Itay Wolfish <itay.wolfish@echohq.com>
2025-08-27 13:06:23 +0300

    nexus alignments

--- echo-terraform-nexus-mirror/outputs.tf
+++ echo-terraform-nexus-mirror/outputs.tf
@@ -6,5 +6,5 @@ output "repository_name" {
 
 output "usage_instructions" {
   description = "Example Docker pull command"
-  value       = var.create ? "docker pull <nexus-host>${coalesce(var.docker_http_port, var.docker_https_port) != null ? ":${coalesce(var.docker_http_port, var.docker_https_port)}" : ""}/${var.repository_name}/static:latest" : null
+  value       = var.create ? "docker pull <nexus-host>${coalesce(var.docker_http_port, var.docker_https_port) != null ? ":${coalesce(var.docker_http_port, var.docker_https_port)}" : ""}/static:latest" : null
 }
